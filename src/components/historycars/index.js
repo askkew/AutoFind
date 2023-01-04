4abc31ad74460f145c5cda51981ee02e380a2fc8
Lucas Overbey <lucasaoverbey@gmail.com>
2023-01-04 16:54:09 -0600

    updates to historycards

--- src/components/historycars/index.js
+++ src/components/historycars/index.js
@@ -1,23 +1,25 @@
 import styled from 'styled-components'
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import { Card, CardContent, CardMedia, Button, Typography } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 
 const Historycars = () => {
   return (
-    <Card sx={{ width: 250, height: 250}}>
-        <SearchIcon />
-        <CardMedia
-            component="img"
-            alt="car"
-            height="140"
-            image="../../media/m3.png"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            BMW M3
-            </Typography>
-        </CardContent>
-    </Card>
+    <Button>
+        <Card sx={{ width: 250, height: 250}}>
+            <SearchIcon style = {{marginTop: 10}} />
+            <CardMedia style = {{ width: 250, padding: 10}} 
+                component="img"
+                alt="car"
+                height="140"
+                image= {require ("../../media/m3.png")}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                BMW M3
+                </Typography>
+            </CardContent>
+        </Card>
+    </Button>
   )
 }
 
